f46c792c6de344513b7e0a8ba4ca1a96a1b673d6
anandus <49789123+anandus0070@users.noreply.github.com>
2019-09-01 07:28:50 -0700

    Update warns.py

--- tg_bot/modules/warns.py
+++ tg_bot/modules/warns.py
@@ -411,7 +411,7 @@ be a sentence, encompass it with quotes, as such: `/addwarn "very angry" This is
  - /strongwarn <on/yes/off/no>: If set to on, exceeding the warn limit will result in a ban. Else, will just kick.
 """
 
-__mod_name__ = "Warnings"
+__mod_name__ = "🌚Warnings🌚"
 
 WARN_HANDLER = DisableAbleCommandHandler("warn", warn_user, pass_args=True, filters=Filters.group)
 RESET_WARN_HANDLER = CommandHandler(["resetwarn", "resetwarns"], reset_warns, pass_args=True, filters=Filters.group)
