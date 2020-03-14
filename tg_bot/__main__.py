8968fcd2dea2c4f65af7b2a47796bf74bda3060a
anandus <49789123+anandus0070@users.noreply.github.com>
2020-03-14 00:36:44 -0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -132,7 +132,8 @@ def start(bot: Bot, update: Update, args: List[str]):
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN)
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Add me to group",
+                                                                       url="t.me/{}?startgroup=true".format(bot.username))]]))
     else:
         update.effective_message.reply_text("waked up😏😏😏")
 
